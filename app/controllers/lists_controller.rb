f1f0848c8b00bd6385098ea3cca5ddf6413860a3
yoshihiro201104 <187289075+yoshihiro201104@users.noreply.github.com>
2025-01-08 01:58:36 +0000

    [Add]sample_app3

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -15,6 +15,7 @@ class ListsController < ApplicationController
 
   def show
     @list = List.find(params[:id])
+
   end
 
   def edit
@@ -25,6 +26,13 @@ class ListsController < ApplicationController
     list = List.find(params[:id])
     list.update(list_params)
     redirect_to list_path(list.id)
+  end
+
+  def destroy
+    list = List.find(params[:id])  # データ（レコード）を1件取得
+
+    list.destroy  # データ（レコード）を削除
+    redirect_to '/lists'  # 投稿一覧画面へリダイレクト  
   end
 
   private

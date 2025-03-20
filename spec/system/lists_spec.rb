493e3aa15ed37aad6da5564ea5337cbf80a599f0
yoshihiro201104 <187289075+yoshihiro201104@users.noreply.github.com>
2025-03-20 02:34:02 +0000

    RSpec

--- spec/system/lists_spec.rb
+++ spec/system/lists_spec.rb
@@ -30,36 +30,76 @@ describe '投稿のテスト' do
         expect(page).to have_button '投稿'
       end
     end
-    context：投稿処理のテスト
-      it：投稿後のリダイレクト先は正しいか
-        テストコード
+    context '投稿処理のテスト' do
+      it '投稿後のリダイレクト先は正しいか' do
+        fill_in 'list[title]', with: Faker::Lorem.characters(number:10)
+        fill_in 'list[body]', with: Faker::Lorem.characters(number:30)
+        click_button 'Create List'
+        expect(page).to have_current_path list_path(List.last)
+      end
+    end
+  end
 
-  describe：一覧画面のテスト
-    before：一覧画面への遷移
-    context ：一覧の表示とリンクの確認
-      it：一覧表示画面に投稿されたものが表示されているか
-        テストコード
+  describe '一覧画面のテスト' do
+    before do
+      visit lists_path
+    end
+    context  '表示の確認' do
+      it "投稿されたものが表示されているか" do
+        expect(page).to have_content list.title
+        expect(page).to have_link list.title
+      end
+    end
+  end
 
-  describe：詳細画面のテスト
-    before：詳細画面への遷移
-    context：表示の確認
-      it：削除リンクが存在しているか
-        テストコード
-      it：編集リンクが存在しているか
-        テストコード
-    context：リンクの遷移先の確認
-      it：編集の遷移先は編集画面か
-        テストコード
-    context：list削除のテスト
-      it：listの削除
-        テストコード 
+  describe '詳細画面のテスト' do
+    before do
+      visit list_path(list)
+    end
+    context '表示の確認' do
+      it '削除リンクが存在しているか' do
+        expect(page).to have_link '削除'
+      end
+    end
+    it '編集リンクが存在しているか' do
+      expect(page).to have_link '編集'
+    end
+  end
+      context 'リンクの遷移先の確認'
+      it '編集の遷移先は編集画面か' do
+       edit_link = find_all('a')[3]
+       edit_link.click
+       expect(current_path).to eq('/lists/' + list.id.to_s + '/edit')
+       end
+      end
+    context 'list削除のテスト' do
+      it 'listの削除' do
+        expect{ list.destroy }.to change{ List.count }.by(-1)
+      end
+    end
+  end
         
-  describe：編集画面のテスト
-    before：編集画面への遷移
-    context：表示の確認
-      it：編集前のタイトルと本文がフォームに表示(セット)されている
-        テストコード
-      it：保存ボタンが表示される
-    context：更新処理に関するテスト
-      it：更新後のリダイレクト先は正しいか
-        テストコード
+  describe '編集画面のテスト' do
+    before do
+      visit edit_list_path(list)
+    end
+    context '表示の確認' do
+      it '編集前のタイトルと本文がフォームに表示(セット)されている' do
+       expect(page).to have_field 'list[title]', with: list.title
+       expect(page).to have_field 'list[body]', with: list.body
+     end
+
+      it '保存ボタンが表示される' do
+        expect(page).to have_button '保存'
+      end
+    end
+    context '更新処理に関するテスト' do
+      it '更新後のリダイレクト先は正しいか' do
+       fill_in 'list[title]', with: Faker::Lorem.characters(number:10)
+       fill_in 'list[body]', with: Faker::Lorem.characters(number:30)
+       click_button 'Update List'
+       expect(page).to have_current_path book_path(list)
+      end
+    end
+  end
+end
